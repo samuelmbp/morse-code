3356abc5387efd0ddacd5fd8316350c8bf2dd183
Samuel Raducan <samuel.thembp@gmail.com>
2025-07-14 20:13:42 +0200

    Add morse code mapping

--- src/main/scala/InputHandler.scala
+++ src/main/scala/InputHandler.scala
@@ -2,6 +2,7 @@ import scala.io.StdIn.readLine
 
 class InputHandler {
   def readInput(input: String): String  = {
+    // TODO: Add input validation
     print(input)
     readLine()
   }
--- src/main/scala/Translator.scala
+++ src/main/scala/Translator.scala
@@ -1,9 +1,14 @@
 
-object Translator extends App {
+object Translator {
 
- val inputHandler = new InputHandler()
- val outputHandler = new OutputHandler()
+ def main(args: Array[String]): Unit = {
+  val inputHandler = new InputHandler()
+  val outputHandler = new OutputHandler()
+  val morseCode = new MorseCode()
 
- val message = inputHandler.readInput("Please enter a message: ")
- outputHandler.printOutput(s"Your message is: $message")
+  val message = inputHandler.readInput("Please enter a message: ")
+  outputHandler.printOutput(s"Your message is: $message")
+
+  println(morseCode.morseCode)
+ }
 }

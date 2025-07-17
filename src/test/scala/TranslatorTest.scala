3ba946c44bfafaffa9a8ed1fce5a8e13737c9db8
Samuel Raducan <samuel.thembp@gmail.com>
2025-07-17 10:18:38 +0200

    Add multiple unit tests for englishToMorse and morseToEnglish functionality

--- src/test/scala/TranslatorTest.scala
+++ src/test/scala/TranslatorTest.scala
@@ -1,18 +1,69 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class TranslatorTest extends AnyFunSuite {
-  
-  test("should transform the sentence into Morse") {
+
+  test("should translate the sentence into Morse") {
     val sentence = "London"
     val morse = new MorseCode
     assert(Translator.englishToMorse(sentence, morse.morseCode) == ".-.. --- -. -.. --- -." )
   }
-  
+
   test("should throw an error when sentence is empty") {
     val sentence = ""
     val morse = new MorseCode
     assertThrows[IllegalArgumentException] {
       Translator.englishToMorse(sentence, morse.morseCode)
     }
+  }
+
+  test("should translate from Morse to a sentence") {
+    val morseCodeSentence = "--. --- --- -.. / .-.. .. ..-. ."
+    val morse = new MorseCode
+    assert(Translator.morseToEnglish(morseCodeSentence, morse.morseCode) == "GOOD LIFE")
+  }
+
+  test("should throw an error when Morse sentence is empty") {
+    val emptyMorseCode = ""
+    val morse = new MorseCode
+    assertThrows[IllegalArgumentException] {
+      Translator.morseToEnglish(emptyMorseCode, morse.morseCode)
+    }
+  }
+
+  test("should translate sentence with punctuation correctly") {
+    val sentence = "HELLO WORLD."
+    val morse = new MorseCode
+    val result = Translator.englishToMorse(sentence, morse.morseCode)
+    assert(result.contains(".-.-.-"))
+  }
+
+  test("should translate sentence with numbers correctly") {
+    val sentence = "HELLO WORLD10"
+    val morse = new MorseCode
+    val result = Translator.englishToMorse(sentence, morse.morseCode)
+    assert(result.contains(".---- -----"))
+  }
+
+  test("should translate English to Morse and back consistently") {
+    val sentence = "GOOD LIFE"
+    val morse = new MorseCode
+    val morseCode = Translator.englishToMorse(sentence, morse.morseCode)
+    val backToEnglish = Translator.morseToEnglish(morseCode, morse.morseCode)
+    assert(backToEnglish == sentence)
+  }
+
+  test("should handle multiple spaces") {
+    val sentence = "GOOD  LIFE"
+    val morse = new MorseCode
+    val morseCode = Translator.englishToMorse(sentence, morse.morseCode)
+    assert(morseCode.contains("/"))
+  }
+
+  test("should lowercase input correctly") {
+    val sentence = "good life"
+    val morse = new MorseCode
+    val morseCode = Translator.englishToMorse(sentence, morse.morseCode)
+    val backToEnglish = Translator.morseToEnglish(morseCode, morse.morseCode)
+    assert(backToEnglish == "GOOD LIFE")
   }
 }
